89f42cd30b08b8d2965f69a9c8acc0b1edc13800
mctaylors <cantsendmails@mctaylors.ru>
2026-03-08 18:50:35 +0300

    change: rebrand to PineconeMC
    Signed-off-by: mctaylors <cantsendmails@mctaylors.ru>

--- program_info/README.md
+++ program_info/README.md
@@ -1,6 +1,6 @@
-# Prism Launcher Program Info
+# PineconeMC Program Info
 
-This is Prism Launcher's program info which contains information about:
+This is PineconeMC's program info which contains information about:
 
 - Application name and logo (and branding in general)
 - Various URLs and API endpoints
--- program_info/genicons.sh
+++ program_info/genicons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LAUNCHER_APPID="org.prismlauncher.PrismLauncher"
+LAUNCHER_APPID="ru.pineconemc.launcher"
 
 svg2png() {
     input_file="$1"
@@ -15,24 +15,24 @@ if command -v "inkscape" && command -v "icotool" && command -v "oxipng"; then
     # Windows ICO
     d=$(mktemp -d)
 
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_16.png" 16 16
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_24.png" 24 24
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_32.png" 32 32
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_48.png" 48 48
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_64.png" 64 64
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_128.png" 128 128
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_256.png" 256 256
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_16.png" 16 16
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_24.png" 24 24
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_32.png" 32 32
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_48.png" 48 48
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_64.png" 64 64
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_128.png" 128 128
+    svg2png ${LAUNCHER_APPID}.svg "$d/pineconemc_256.png" 256 256
 
-    oxipng --opt max --strip all --alpha --interlace 0 "$d/prismlauncher_"*".png"
+    oxipng --opt max --strip all --alpha --interlace 0 "$d/pineconemc_"*".png"
 
-    rm prismlauncher.ico && icotool -o prismlauncher.ico -c \
-        "$d/prismlauncher_256.png"  \
-        "$d/prismlauncher_128.png"  \
-        "$d/prismlauncher_64.png"   \
-        "$d/prismlauncher_48.png"   \
-        "$d/prismlauncher_32.png"   \
-        "$d/prismlauncher_24.png"   \
-        "$d/prismlauncher_16.png"
+    rm pineconemc.ico && icotool -o pineconemc.ico -c \
+        "$d/pineconemc_256.png"  \
+        "$d/pineconemc_128.png"  \
+        "$d/pineconemc_64.png"   \
+        "$d/pineconemc_48.png"   \
+        "$d/pineconemc_32.png"   \
+        "$d/pineconemc_24.png"   \
+        "$d/pineconemc_16.png"
 else
     echo "ERROR: Windows icons were NOT generated!" >&2
     echo "ERROR: requires inkscape, icotool and oxipng in PATH"
@@ -42,7 +42,7 @@ if command -v "inkscape" && command -v "iconutil" && command -v "oxipng"; then
     # macOS ICNS
     d=$(mktemp -d)
 
-    d="$d/prismlauncher.iconset"
+    d="$d/pineconemc.iconset"
 
     mkdir -p "$d"
 
@@ -60,7 +60,7 @@ if command -v "inkscape" && command -v "iconutil" && command -v "oxipng"; then
     oxipng --opt max --strip all --alpha --interlace 0 "$d/icon_"*".png"
 
     iconutil -c icns "$d"
-    cp -v "$d/prismlauncher.icns" .
+    cp -v "$d/pineconemc.icns" .
 else
     echo "ERROR: macOS icons were NOT generated!" >&2
     echo "ERROR: requires inkscape, iconutil and oxipng in PATH"
