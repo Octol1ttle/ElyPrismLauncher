1ae43fdd8cb70dee8260f88fd8738ced52fc42c6
Octol1ttle <l1ttleofficial@outlook.com>
2026-03-11 23:22:37 +0500

    fix CMake load errors & compilation errors
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
             Q_INIT_RESOURCE(multimc);
             Q_INIT_RESOURCE(backgrounds);
             Q_INIT_RESOURCE(documents);
-            Q_INIT_RESOURCE(prismlauncher);
+            Q_INIT_RESOURCE(pineconemc);
 
             Q_INIT_RESOURCE(pe_dark);
             Q_INIT_RESOURCE(pe_light);
