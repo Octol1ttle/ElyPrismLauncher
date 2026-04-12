6e8c5b41caaf0565ac5c5ead65131e37d2c89dcf
Octol1ttle <l1ttleofficial@outlook.com>
2026-04-12 22:03:09 +0500

    JavaPerformance: add extra, but still safe flags
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- launcher/java/JavaPerformance.cpp
+++ launcher/java/JavaPerformance.cpp
@@ -25,7 +25,7 @@ QStringList JavaPerformance::getMojangOptimizationArgs(const JavaVersion& versio
 {
     QStringList args;
 
-    // -XX:+UseCompactObjectHeaders
+    // Mojang default
     // JEPs: https://openjdk.org/jeps/450, https://openjdk.org/jeps/519
     if (version.major() >= 24) {
         if (version.major() == 24) {
@@ -34,16 +34,28 @@ QStringList JavaPerformance::getMojangOptimizationArgs(const JavaVersion& versio
         args << "-XX:+UseCompactObjectHeaders";
     }
 
-    // -XX:+AlwaysPreTouch
-    // Since Java 6 (minimum supported by us is Java 7)
-    args << "-XX:+AlwaysPreTouch";
-
-    // -XX:+UseStringDeduplication
+    // Mojang default
     // Since Java 8 for G1GC
     // Since Java 18 for ZGC
     if ((version.major() >= 8 && preset == GarbageCollectorPreset::G1GC) || version.major() >= 18) {
         args << "-XX:+UseStringDeduplication";
     }
+
+    // Enables parallel GC reference processing
+    // https://bugs.openjdk.org/browse/JDK-8359925
+    // Since Java 6
+    if (version.major() <= 25) {
+        args << "-XX:+ParallelRefProcEnabled";
+    }
+
+    // Mojang default
+    // Since Java 6 (minimum supported by us is Java 7)
+    args << "-XX:+AlwaysPreTouch";
+
+    // Prevents GC from doing writing performance data to filesystem
+    // https://www.evanjones.ca/jvm-mmap-pause.html
+    // Since Java 6
+    args << "-XX:+PerfDisableSharedMem";
 
     return args;
 }
@@ -53,24 +65,37 @@ QStringList JavaPerformance::getGarbageCollectorArgs(const JavaVersion& version,
     switch (preset) {
         case GarbageCollectorPreset::None:
             return {};
-        case GarbageCollectorPreset::G1GC:
-            return { "-XX:+UnlockExperimentalVMOptions", "-XX:+UseG1GC",
-                     "-XX:G1NewSizePercent=20",          "-XX:G1ReservePercent=20",
-                     "-XX:MaxGCPauseMillis=50",          "-XX:G1HeapRegionSize=32M" };
-        case GarbageCollectorPreset::ZGC:
+        case GarbageCollectorPreset::G1GC: {
+            // Mojang defaults
+            QStringList args{ "-XX:+UnlockExperimentalVMOptions", "-XX:+UseG1GC",
+                              "-XX:G1NewSizePercent=20", "-XX:G1ReservePercent=20",
+                              "-XX:MaxGCPauseMillis=50", "-XX:G1HeapRegionSize=32M",
+                              // Aikar's flags
+                              "-XX:SurvivorRatio=32", "-XX:MaxTenuringThreshold=1" };
+            return args;
+        }
+        case GarbageCollectorPreset::ZGC: {
+            // Mojang defaults
             QStringList args{ "-XX:+UseZGC" };
             // Support for generations was added in Java 21 and became default in Java 23
             if (version.major() >= 21 && version.major() < 23) {
                 args << "-XX:+ZGenerational";
             }
+            // https://docs.oracle.com/en/java/javase/21/gctuning/z-garbage-collector.html
+            // Prevent ZGC from returning memory to the OS, as this might have a negative impact on the latency of Java threads.
+            args << "-XX:-ZUncommit";
             return args;
+        }
     }
 
     Q_ASSERT_X(false, "JavaPerformance::getGarbageCollectorArgs", "No arguments specified for current garbage collector preset");
     return {};
 }
 
-QStringList JavaPerformance::getCompletePerformanceArgs(const JavaVersion& version, const bool useOptimized, GarbageCollectorPreset preset, QString* warning)
+QStringList JavaPerformance::getCompletePerformanceArgs(const JavaVersion& version,
+                                                        const bool useOptimized,
+                                                        GarbageCollectorPreset preset,
+                                                        QString* warning)
 {
     // ZGC was declared as production ready in Java 15, but did not receive support for macOS/aarch64 until Java 17
     if (preset == GarbageCollectorPreset::ZGC && version.major() < 17) {
